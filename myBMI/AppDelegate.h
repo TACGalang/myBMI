33b834c17554879076c26e1b636fa7822c0cc9d1
TACGalang <tristan.galang@live.com>
2017-05-27 10:50:48 +0800

    Initial Commit

---
+++ myBMI/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  myBMI
+//
+//  Created by Irish Magtalas on 27/05/2017.
+//  Copyright © 2017 Tristan Galang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ myBMI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myBMI
+//
+//  Created by Irish Magtalas on 27/05/2017.
+//  Copyright © 2017 Tristan Galang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
